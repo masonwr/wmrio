d95aa1e31459eee9d6421d75f6ec28e44926a17a
masonwr <winston.m.riley@gmail.com>
2021-05-24 08:56:56 -0600

    Add basic support for static assets
    - driveby: restructure directories

--- src/serve/main.rs
+++ src/serve/main.rs
@@ -1,3 +1,5 @@
+use std::os::unix::fs::symlink;
+use std::path::Path;
 use std::{env, fs};
 use tera::{Context, Tera};
 
@@ -6,18 +8,25 @@ async fn main() -> anyhow::Result<()> {
     let project_root = env::var("CARGO_MANIFEST_DIR")?;
     env::set_current_dir(&project_root)?;
 
-    let template_path = format!("{}/templates/default/**/*.html", project_root);
+    let theme_path = format!("{}/themes/default", &project_root);
+    let template_path = format!("{}/templates/**/*.html", theme_path);
     let tera = Tera::new(&template_path)?;
 
     let mut context = Context::new();
     context.insert("title", "wmrio");
-    context.insert("world", "world");
+    context.insert("world", "World");
 
     let rendered = tera.render("index.html", &context)?;
 
     // write out, and then server
     fs::create_dir_all("target/site")?;
     fs::write("target/site/index.html", rendered)?;
+    let theme_static_path = format!("{}/static", theme_path);
+    let build_static_path = "target/site/static";
+
+    if !Path::new(build_static_path).exists() {
+        symlink(&theme_static_path, build_static_path)?;
+    }
 
     println!("listening on 'http://localhost:3030'");
 

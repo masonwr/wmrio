207d70ccb2e484c2aa8495f4d3f8a070ee213192
masonwr <winston.m.riley@gmail.com>
2021-05-24 08:39:50 -0600

    add watch script

--- src/serve/main.rs
+++ src/serve/main.rs
@@ -4,23 +4,22 @@ use tera::{Context, Tera};
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let project_root = env::var("CARGO_MANIFEST_DIR")?;
-    // todo cd to project root
+    env::set_current_dir(&project_root)?;
 
     let template_path = format!("{}/templates/default/**/*.html", project_root);
-
     let tera = Tera::new(&template_path)?;
 
     let mut context = Context::new();
     context.insert("title", "wmrio");
+    context.insert("world", "world");
 
     let rendered = tera.render("index.html", &context)?;
 
-    println!("{:?}", rendered);
-
+    // write out, and then server
     fs::create_dir_all("target/site")?;
     fs::write("target/site/index.html", rendered)?;
 
-    // write out, and then server
+    println!("listening on 'http://localhost:3030'");
 
     warp::serve(warp::fs::dir("target/site"))
         .run(([127, 0, 0, 1], 3030))
